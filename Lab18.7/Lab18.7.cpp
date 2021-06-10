300acc47f1e5bd5259c6ca4bd4dd8f34e4676522
Larra Padla <artem1978354@gmail.com>
2021-06-10 15:35:45 +0500

    qq

--- Lab18.7/Lab18.7.cpp
+++ Lab18.7/Lab18.7.cpp
@@ -5,17 +5,19 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "ru");
-    cout << "Введите 1ю сумму:";
-    Money S1, S2;
-    cin >> S1;
-    cout << "Введите 2ю сумму:";
-    cin >> S2;
-    cout << "1я сумма:";
-    cout << S1;
-    cout << "\n2я сумма:";
-    cout << S2;
-    container<Money>A(4);
+    //   cout << "Введите 1ю сумму:";
+    //   Money S1, S2;
+    //   cin >> S1;
+    //   cout << "Введите 2ю сумму:";
+    //   cin >> S2;
+    //   cout << "1я сумма:";
+     //cout << S1;
+     //cout << "\n2я сумма:";
+     //cout << S2;
+    container<Money> A(4);
+    cin >> A;
     cout << A;
+    cout << endl;
     cout << A[2];
     return 0;
 }
--- Lab18.7/container.h
+++ Lab18.7/container.h
@@ -5,17 +5,104 @@ template <class T>
 class container
 {
 public:
-	container(int size);
-	container(const container<T>& obj);
-	~container();
-	container& operator = (const container<T>& obj);
-	int& operator [] (const int index);
-	friend ostream& operator << (ostream& out, const container<T>& other);
-	friend istream& operator >> (istream& out, const container<T>& other);
-	bool operator != (const container<T>& obj);
-	bool operator < (const int value);
+    container(int size)
+    {
+        this->size = size;
+        data = new T[size];
+    }
+    container(const container& obj)
+    {
+        this->size = obj.size;
+        data = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = obj.data[i];
+        }
+    }
+    ~container()
+    {
+        delete[] data;
+        data = nullptr;
+    }
+    container<T>& operator = (const container<T>& obj)
+    {
+        if (this == &obj)
+        {
+            return *this;
+        }
+        this->size = obj.size;
+        if (data != 0)
+        {
+            delete[]data;
+        }
+        data = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = obj.data[i];
+        }
+        return *this;
+    }
+    T& operator [] (const int index)
+    {
+        if (index < size)
+        {
+            return data[index];
+        }
+        else
+        {
+            cout << "ќшибка!";
+        }
+    }
+    bool operator != (const container& obj)
+    {
+        if (this->size == obj.size)
+        {
+            bool tmp = false;
+            for (int i = 0; i < size; i++)
+            {
+                if (data[i] != obj.data[i])
+                {
+                    tmp = true;
+                }
+            }
+            return tmp;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    bool operator < (const int value)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (data[i] == value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    friend ostream& operator<< (ostream& out, const container<T>& other)
+    {
+        for (int i = 0; i < other.size; i++)
+        {
+            out << other.data[i] << "  ";
+        }
+        return out;
+    }
+    friend istream& operator>> (istream& in, container<T>& other)
+    {
+        for (int i = 0; i < other.size; i++)
+        {
+            cout << "¬ведите параметр:";
+            in >> other.data[i];
+        }
+        return in;
+    }
+
 private:
-	int size;
-	int* data;
+    int size;
+    T* data;
 
 };
